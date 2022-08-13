e6f57f6f720ae5ce92335af28ca7f09f28b8492e
Dalan Cox <dalanjcox@gmail.com>
2022-08-12 20:18:56 -0600

    cors

--- src/util/Yelp.js
+++ src/util/Yelp.js
@@ -2,10 +2,9 @@ const apikey = 'O54GZMe08RdJyvSxH9MGOrw0eGxCl5Va84Z-pWQvqih_3CbfEa8Jv-MIXm4zX1Xy
 
 const Yelp = {
     searchYelp(term, location, sortBy) {
-        return fetch(`https://api.yelp.com/v3/businesses/search?term=${term}&location=${location}&sort_by=${sortBy}`, {
+        return fetch(`https://cors-anywhere.herokuapp.com/https://api.yelp.com/v3/businesses/search?term=${term}&location=${location}&sort_by=${sortBy}`, {
             headers: {
-                Authorization: `Bearer ${apikey}`,
-                mode: `no-cors`
+                Authorization: `Bearer ${apikey}`
             },
         }).then((response) => {
             return response.json();
